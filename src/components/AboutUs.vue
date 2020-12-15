5033b38a61d531ac246a78e000ff33dce8842b8f
agoldh20 <a.goldh20@gmail.com>
2020-12-15 12:26:24 -0600

    inital commit

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+   <div class="card mb-3" style="max-width: 540px;">
+     <div class="row no-gutters">
+       <div class="col-md-4">
+         <img src="../assets/logo.png" class="card-img" alt="...">
+       </div>
+       <div class="col-md-8">
+         <div class="card-body">
+           <h5 class="card-title">Robert Goldstein PT, DPT</h5>
+           <p class="card-text">Robert Goldstein is a Physical Therapy Specialist in Skokie, Illinois. He graduated with honors in 2002. Having more than 18 years of diverse experiences, especially in physical therapy. The NPI Number for Robert Goldstein is 1780997478 and he holds a License No. 070013313 (Illinois).</p>
+         </div>
+       </div>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutUs"
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <AboutUs />
+</template>
+
+<script>
+// @ is an alias to /src
+import AboutUs from "@/components/AboutUs.vue";
+
+export default {
+  name: "About",
+  components: {
+    AboutUs
+  }
+};
+</script>
