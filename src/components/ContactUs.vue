b8e8d8cae74351b6d286139a84b038473f1f5a7d
agoldh20 <a.goldh20@gmail.com>
2021-01-03 23:38:56 -0600

    adds contact page

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <br>
+    <h5>
+      Contact us today
+    </h5>
+    <div class="card-group">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Phone</h5>
+          <a href="tel:847-312-2238">&nbsp;(847) 312-2238&nbsp;</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Fax</h5>
+          <a href="tel:847-329-7215">&nbsp;(847) 329-7215&nbsp;</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Email</h5>
+          <a href="mailTo:info@goldrehabservices.com">info@goldrehabservices.com</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
