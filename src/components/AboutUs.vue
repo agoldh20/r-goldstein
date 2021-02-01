0b73c3af8797d35f2296efa39b396db936dbbba8
agoldh20 <a.goldh20@gmail.com>
2021-02-01 00:08:35 -0600

    updates bio and pic

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -5,19 +5,24 @@
      <div class="card mb-3 mx-auto" style="max-width: 95%;">
        <div class="row no-gutters">
          <div class="col-md-4">
-           <img src="@/assets/Robert_Downey_Jr.jpg" class="card-img" alt="Robert Goldstein">
+           <img src="@/assets/robert_goldstein_headshot.jpg" class="card-img" alt="Robert Goldstein">
          </div>
          <div class="col-md-8">
            <div class="card-body">
              <h5 class="card-title">Robert Goldstein PT, DPT</h5>
-             <p class="card-text text-left">Robert received his Bachelor's Degree in Exercise Physiology from University of Illinois at Chicago and received his Doctor of Physical Therapy degree from Rosalind Franklin University/Chicago Medical School. <br><br>
-              Robert has 18+ experience in both outpatient orthopedics and home health care treating a wide variety of orthopedic, sports, neurologic, and pain-related conditions amongst all age groups. His approach is to use both his exercise science training background along with his manual therapy expertise to return patients to the activities they love.  Robert has experience in manual therapy, cupping, functional dry needling (certified), instrument assisted soft tissue mobilization, Mulligan Manual Therapy techniques, and kinesio taping.  Robert's ability to get his patients from the most acute phases of an injury or from post-surgical recovery back to their prior level of mobility in a safe and efficient manner is one of his many skills that set him apart. <br><br>
-              Robert has the unique ability to understand the Physical Therapy rehab process from the patient's perspective, as he has also been a patient, including during recovery from ACL reconstruction surgery.  Robert has a very caring personality and takes a sympathetic and empathetic approach to all patients, as his goal is to ensure he and his patients are working together to reach their goals.  Priding himself on personally investing in every patient he meets, Robert works to create individualized plans that take into account each patient and injury's unique needs.
+             <p class="card-text text-left">
+              Robert received his bachelor's degree in exercises physiology from University of Illinois at Chicago and graduated with his Doctor of Physical Therapy in 2002 from Roslyn Franklin University/Chicago Medical School. 
+              <br>
+              <br>
+              Robert has 18+ experience in both outpatient orthopedics and home health care treating a wide variety of orthopedic, sports, neurologic and pain related conditions from young adults to geriatric populations. His approach is to use both his exercise science training background with manual therapy to return patients to the activities they love. Robert has experience in cupping, manual therapy, functional dry needling certification, instrument assisted soft tissue mobilization, mulligan manual therapy techniques and kinesio taping. Robert's experience can take his patient from the most acute phase of injury or post surgery to return back to prior level of function is one of the many abilities that sets him apart. 
+              <br>
+              <br>
+              Robert understands physical therapy rehab process from the perspective of the patient, as well, including being a patient a few times himself including recovery from ACL reconstruction surgery. His personality and approact for his patients are both sympathetic and empathetic, making his clients feel as they are working together to acheive you goal. Priding himself on personality ivesting in every patient he meets, Robert works to create individualized plans taking into account all of each patient's unique factors.
             </p>
           </div>
         </div>
       </div>
-    </div>
+    </div>`
   </div>
 </div>
 </template>
