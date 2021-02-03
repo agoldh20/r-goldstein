a915bc2c026a857761c20a964c5bd93111ba4aa5
agoldh20 <a.goldh20@gmail.com>
2021-02-03 16:54:08 -0600

    fixes mapbox

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Contact from "./views/Contact.vue";
+import Location from "./views/Location.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: Contact
+    },
+    {
+      path: "/location",
+      name: "location",
+      component: Location
+    }
+  ]
+});
+
+export default router;
