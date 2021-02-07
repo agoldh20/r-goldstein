65bf53668651612608d93b5b597301a480861653
agoldh20 <a.goldh20@gmail.com>
2021-02-07 00:04:44 -0600

    adds forms page

---
+++ src/views/Forms.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <br>
+    <h5>
+      Forms
+    </h5>
+    <a href="/forms/[...]" download></a>
+    <h3 class="coming-soon">
+      *** Downloadable Forms will be added soon ***
+    </h3>
+  </div>
+</template>
+<style>
+.coming-soon {
+  font-weight: bolder;
+  color: blue;
+}
+</style>
