83da422e3ef8774014cc487a46bb855ff29f87c8
agoldh20 <agoldh20@gmail.com>
2021-04-01 22:22:59 -0500

    seo prerender

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+app.use(require('prerender-node').set('prerenderToken', process.env.PRETENDER_TOKEN
+
 
 Vue.config.productionTip = false;
 
